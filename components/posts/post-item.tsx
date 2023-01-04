1ef233db91e382e4b7291f28e3f6b0110f06a8db
nolann7 <nolann709@gmail.com>
2023-01-04 14:00:46 +0200

    adding linkPath

--- components/posts/post-item.tsx
+++ components/posts/post-item.tsx
@@ -16,12 +16,14 @@ const PostItem = ({ post }: PostItemProps) => {
     month: 'long',
     year: 'numeric',
   });
-  const imagePath = `/images/posts/${slug}/${image}`
+  const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
+    
   return (
     <li className={classes.post}>
-      <Link href={slug}>
+      <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200}/>
+          <Image src={imagePath} alt={title} width={300} height={200} />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>
